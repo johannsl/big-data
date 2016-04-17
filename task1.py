ecf574c83cda8fac713b0ea1bf5f40bad9c81de6
johannsl <johannsl@stud.ntnu.no>
2016-04-17 19:33:23 +0200

    timeoffset calculation works

--- task1.py
+++ task1.py
@@ -12,13 +12,22 @@ sc = SparkContext(conf = conf)
 foursquare_data = sc.textFile("./foursquare-data/dataset_TIST2015.tsv")
 cities_data = sc.textFile("./twitter-data/dataset_TIST2015_Cities.txt")
 
-def printer(data):
-    print data
+def set_time(data):
+    line = data.split("\t")
+    old_date = datetime.strptime(line[3], "%Y-%m-%d %H:%M:%S")
+    delta = int(line[4])
+    new_date = datetime.strftime(old_date + timedelta(minutes=delta),
+                "%Y-%m-%d %H:%M:%S")
+    line[3] = new_date
+    line[4] = "0"
+    result = "\t".join(line)
+    return result
 
-# Prints all elements from foresquare
-foursquare_data.foreach(printer)
-
-
+header = foursquare_data.first()
+headless = foursquare_data.filter(lambda x: x != header)
+new_rdd = headless.map(set_time)
+new_rdd.saveAsTextFile("foursquare-data/foursquare_edit")
+#new_rdd.count()
 
 # To be worked on
 #def time_printer(data):
