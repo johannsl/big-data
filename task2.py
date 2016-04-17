b358aefc5703e4ea9d9504a6945ac4804610f18c
johannsl <johannsl@stud.ntnu.no>
2016-04-17 21:14:36 +0200

    Created a general progression in task2.py

--- task2.py
+++ task2.py
@@ -9,12 +9,31 @@ from datetime import timedelta
 from pyspark import SparkConf
 from pyspark import SparkContext
 
+# Create a spark context
 conf = (SparkConf()
          .setMaster("local")
          .setAppName("My app")
          .set("spark.executor.memory", "1g"))
 sc = SparkContext(conf = conf)
 
+# Print some SparkConf variables
+print "\nSparkConf variables: \n", conf.toDebugString()
+
+# Import data
 foursquare_data = sc.textFile("foursquare-data/foursquare_edit/part-00000")
 
-foursquare_data.saveAsHadoopDataset("foursquare/hadoop_dataset")
+# Provoke action in dataset
+data_type = type(foursquare_data) 
+first = foursquare_data.first()
+count = foursquare_data.count()
+
+# Print some data information
+print "\nfoursquare_data filetype: ", data_type
+print "\nFirst element in the dataset: ", first
+print "\nNumber of elements: ", count
+
+# Save the data
+#foursquare_data.saveAsHadoopDataset("foursquare/hadoop_dataset")
+
+# Stop the spark context
+sc.stop()
