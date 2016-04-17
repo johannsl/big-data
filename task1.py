f2f7fb534c5f55ff8bed2e7968494e8bfdcadb27
johannsl <johannsl@stud.ntnu.no>
2016-04-17 20:16:01 +0200

    task2.py created for the next project task

--- task1.py
+++ task1.py
@@ -1,3 +1,9 @@
+"""
+Imports the forsquare dataset and does the time calculation,
+it saves the output as many different partitions (in my case, 58 partitions).
+- johan
+"""
+
 from datetime import datetime
 from datetime import timedelta
 from pyspark import SparkConf
@@ -27,38 +33,3 @@ header = foursquare_data.first()
 headless = foursquare_data.filter(lambda x: x != header)
 new_rdd = headless.map(set_time)
 new_rdd.saveAsTextFile("foursquare-data/foursquare_edit")
-#new_rdd.count()
-
-# To be worked on
-#def time_printer(data):
-#    NotImplemented 
-#
-#def time_calc(data):
-#    date = datetime.strptime(data.split("\t")[3],'%Y-%m-%d %H:%M:%S')
-#    delta = int(data.split("\t")[4])
-#    total = datetime.strftime(date + timedelta(minutes=delta))
-#    
-#    
-#
-#header = data0.first()
-#data0 = data0.filter(lambda x:x !=header)
-#
-
-#data0.foreach(time_printer)
-#data0.foreach(time_calc)
-
-#temp0 = temp0.take(10)
-
-#for line in temp0:
-	#columns = line.split("\t")
-	#time0 = time_calc(columns[3])
-	#print temp0
-	#print time0
-	#time1 = time0 + timedelta(0, int(columns[4])*60)
-	#print int(columns[4])/60
-	#print time1
-	#new_times[columns[0]] = time1
-#print new_times
-
-#with open("./times.json", "w") as f:
-#	json.dump(new_times, f)
